d85cc4cc3fbf45fea375dc6ee1e0d13e84c8c87e
eren <erik.raik.engelhardt@gmail.com>
2024-09-18 15:31:43 +0200

    Got a docker version running.

--- personal-website/src/main.rs
+++ personal-website/src/main.rs
@@ -7,7 +7,7 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=debug");
     env_logger::init();
 
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    let listener = TcpListener::bind("0.0.0.0:8080")?;
     start_blog(listener)?.await?;
     Ok(())
 }
